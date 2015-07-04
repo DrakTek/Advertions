cbc53e29e59eaa702a371dd54cf71f0d8bf6f870
Drak <ddiawara@outlook.com>
2015-07-04 09:00:48 +0000

    atom install

--- src/Drak/BlogBundle/Controller/AdvertController.php
+++ src/Drak/BlogBundle/Controller/AdvertController.php
@@ -1,2 +1,13 @@
 <?php
-    
+
+    namespace Drak\BlogBundle\Controller;
+
+    use Symfony\Component\HttpFoundation\Response;
+
+    class AdvertController
+    {
+        public function indexAction()
+        {
+            return new Response("Hello Drak !");
+        }
+    }
