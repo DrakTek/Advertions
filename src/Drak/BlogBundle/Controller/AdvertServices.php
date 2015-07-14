fa0db257388a4d41c104fdf347f50dfb009a8405
DRAKUN <dj-tung@hotmail.com>
2015-07-15 00:45:23 +0200

    premier test de service

--- src/Drak/BlogBundle/Controller/AdvertServices.php
+++ src/Drak/BlogBundle/Controller/AdvertServices.php
@@ -9,18 +9,19 @@
 	*/
 	class AdvertServices
 	{
-        private $entityManager;
+        private $em;
         private $days;
 
         public function __construct($days, EntityManager $entityManager)
 		{
-		      $this->entityManager = $entityManager;
+		      // $this->entityManager = $entityManager;
+            $this->em = $entityManager;
 			 $this->days = $days;
 		}
 
 		public function toPurge()
 		{
-			$em = $this->entityManager;
+			// $em = $this->entityManager;
             $list_a_purger = $em
                 ->getRepository('DrakBlogBundle:Advert')
                 ->getliste_purge($days);
